ce63558f61114a90e0745788303a54effa6e18f1
Pawid-prianto27 <pawitanto27@gmail.com>
2019-08-08 13:13:28 +0700

    task array

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 var express = require('express')
 var app = express()
+const array_lib = require("./lib/array")
+const object_lib = require("./lib/object")
+const task_lib = require("./lib/task")
+
+
+//   List of imported routes
+
+const array_routes = require('./routes/array')
+
 
 // for passin application/x-www-form-urlencoded
 app.use(express.urlencoded({extended: true}))
@@ -65,6 +74,63 @@ app.post('/kali', (req, res) => {
      
      return res.send('jumlahnya ' + hasil)
  })
+ app.get('/example-1', (req, res) => {
+    let data = ["Red", "Blue", "Yellow", "Green"]
+    let manipulation = array_lib(data)
+
+    return res.send(manipulation)
+})
+
+app.get('/example-2', (req, res) => {
+    let data = {
+        firstname: "John",
+        lastname: "Doe",
+        email: "john.doe@mail.com",
+        gender: "men"
+    }
+    let result = object_lib.manipulate(data)
+
+    return res.send(result)
+})
+
+app.get("/example-3", (req, res) => {
+    let input = ["Black"]
+    let data = ["Red", "Blue", "Yellow", "Green"]
+    console.log("before ", data)
+
+    let result = object_lib.merge(data = data, input = input)
+    console.log("after ", result)
+
+    return res.send(result)
+})
+
+app.get("/example-4", (req, res) => {
+    let empty_data = []
+    console.log("before ", empty_data)
+
+    let color = "Red"
+    object_lib.add(empty_data, color)
+    console.log("after ", empty_data)
+
+    return res.send(empty_data)
+})
+
+app.get('/task-1', (req, res) => {
+    let data = ["Red", "Blue", "Yellow", "Green"]
+    let manipulate = task_lib.manipulate(data)
+
+    return res.send(manipulate)
+})
+
+app.get('/task-2', (req, res) => {
+    let data = ["Red", "Blue", "Yellow", "Green"]
+    let desc = task_lib.kebalik(data)
+
+    return res.send(desc)
+})
+//   Set all routes
+ 
+app.use("/array", array_routes)
 
 app.listen(3000, () => {
     console.log('http://localhost:3000')
