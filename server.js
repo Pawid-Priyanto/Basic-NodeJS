a83cac6726686a22e7a24b547c326e55f46c34c6
Pawid-prianto27 <pawitanto27@gmail.com>
2019-08-09 16:58:24 +0700

    looping

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var app = express();
 const array_lib = require("./lib/array");
 const object_lib = require("./lib/object");
 const task_lib = require("./lib/task");
+const conditional = require("./lib/conditional");
+const task1 = require("./lib/task1")
+const loop_for = require('./lib/loop_for')
+const loop_foreach = require('./lib/loop_foreach')
+const loop_map = require('./lib/loop_map')
+const map_object = require('./lib/loop_map_object')
 
 // listof imported array
 const array_routes = require('./routes/array')
@@ -30,6 +36,7 @@ app.get('/', (req, res) => {
             console.log(i + " Maaf kamu belum beruntung")
         }    
     }
+   
     return res.send(`<h1><center> ${z} </center></h1> <br>`)
 })
 
@@ -104,14 +111,115 @@ app.get('/task1', (req, res) => {
 })
 
 
+
 app.get('/task2', (req, res) => {
     let data = ["Bumi", "Udara", "Tanah", "Api"]
     let result = task_lib.kebalik(data)
 
     return res.send(result)
 })
-// ser routes
-app.use("/array", array_routes)
+
+
+/**
+  How to using query parameter:
+  
+  http://your_url:your_port/your_route?q=your_value
+  or type in Query Params when using postman
+  key      | value
+  q        | your_value
+ */
+app.get("/task3", (req, res) => {
+    let q = req.query.q
+    console.log("First value ", q)
+
+    let result = conditional.equal(q)
+
+    return res.send(result)
+})
+
+app.get("/task4", (req, res) => {
+    let today = new Date().getDay()
+    console.log("Today ", today)
+
+    let result = conditional.day(today)
+
+    return res.send(result)
+})
+
+app.post("/task5", (req, res) => {
+    let number = req.body.number
+    console.log("section1 ", typeof number)
+    number = parseInt(number)
+    console.log("section2 ", typeof number)
+
+    let result = conditional.compare(number)
+
+    return res.send(result)
+})
+
+app.get("/task6", (req, res) => {
+    let a = req.query.a
+    let b = req.query.b
+    let c = parseInt(a)
+
+    console.log(typeof c)
+    console.log(typeof b)
+    
+
+    let result = task1.chek(c,b)
+
+    return res.send(result)
+})
+app.post('/task7', (req, res) =>{
+    let {name, email} = req.body
+    let result = task1.task2(name, email)
+ 
+    
+   return res.send(result)
+  })
+
+app.get('/loop-for', (req, res) => {
+    let data = ["Red", "Blue", "Green"]
+    let result = loop_for(data)
+
+    return res.send(result)
+})
+
+app.get("/loop-foreach", (req, res) => {
+    let data = ["Red", "Blue", "Green"]
+    let result = loop_foreach(data)
+
+    return res.send(result)
+})
+
+app.get("/loop-map", (req, res) => {
+    let data = ["Red", "Blue", "Green"]
+    let result = loop_map(data)
+
+    return res.send(result)
+})
+// objectloop
+app.get('/object-loop', (req, res) => {
+    let data = [
+        {
+            name: "Red",
+            note: "Danger"
+        },
+        {
+            name: "Yellow",
+            note: "Warning"
+        },
+        {
+            name: "Green",
+            note: "Success"
+        }
+    ]
+
+    let result = map_object(data)
+
+    return res.send(result)
+})
+
 
 app.listen(3300, () => {
     console.log('http://localhost:3300')
